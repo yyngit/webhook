53047b207d2ec5e8e094e7e191fc08ce2ae05043
yyn <934325265@qq.com>
2018-09-20 14:42:28 +0800

    ''

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+from .webhooks import webhook
+
+
+def create_app():
+    '''Create, configure and return the Flask application'''
+    app = Flask(__name__)
+    app.config['GITHUB_SECRET'] = os.environ.get('GITHUB_SECRET')
+    app.config['REPO_PATH'] = os.environ.get('REPO_PATH')
+    app.register_blueprint(webhook)
+    return app
